01b73cc46cb3e07f158fa792e4c54e5e7c719ad8
Enning94 <enningfestus@gmail.com>
2022-09-02 12:50:52 +0000

    implement API functionalities

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-// import _ from 'lodash';
+import 'lodash';
 import './style.css';
+import { postData, nameInput, scoreInput } from './modules/post.js';
+import renderScore from './modules/get.js';
+
+const form = document.querySelector('.input-disp');
+const refreshBtn = document.querySelector('.ref-btn');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postData();
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+refreshBtn.addEventListener('click', () => {
+  renderScore();
+  window.location.reload();
+});
+
+window.onload = renderScore();
